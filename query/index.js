c7bd352a7db7b772095adc71a96e5877db05936a
Sturez <sturez@msn.com>
2023-02-17 17:05:03 +0100

    created query service

---
+++ query/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/posts', (req, res) => {
+
+});
+
+
+app.post('/events', (req, res) => {
+    console.log("Received event:", req.body.type);
+    res.send({});
+});
+
+app.listen(4002, () => {
+    console.log('Listening on 4002');
+});
