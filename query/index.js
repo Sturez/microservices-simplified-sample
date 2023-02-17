3ed63215020634a8c49a3dc9cf1bbdc46ae31fad
Sturez <sturez@msn.com>
2023-02-17 17:22:49 +0100

    added query-service and attached to event bus

--- query/index.js
+++ query/index.js
@@ -7,14 +7,40 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts);
 });
 
 
 app.post('/events', (req, res) => {
     console.log("Received event:", req.body.type);
+    const { type, data } = req.body;
+
+    switch (type) {
+        case "PostCreated":
+            const { id: postid, title } = data;
+            posts[postid] = { id: postid, title, comments: [] };
+
+            break;
+
+        case "CommentCreated":
+            const { id: commentId, content, postId } = data;
+
+            const post = posts[postId];
+            if (post) {
+                post.comments.push({ commentId, content });
+            }
+
+            break;
+
+        default:
+            console.warning("uhnandled event type");
+            break;
+    }
+
     res.send({});
 });
 
