218ec0abc9fda65016b34087ca159dd2e0eed002
Sturez <sturez@msn.com>
2023-02-18 02:19:16 +0100

    added fetching of events when a service goes down

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,17 +6,24 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
 
 app.post('/events', (req, res) => {
     const event = req.body;
     console.log("Received event:", event.type);
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {console.log("post:");console.log(err);});
-    axios.post('http://localhost:4001/events', event).catch((err) => {console.log("comment:");console.log(err);});
-    axios.post('http://localhost:4002/events', event).catch((err) => {console.log("query:");console.log(err);});
-    axios.post('http://localhost:4003/events', event).catch((err) => {console.log("moderator:");console.log(err);});
+    events.push(event);
+
+    axios.post('http://localhost:4000/events', event).catch((err) => { console.log("post:"); console.log(err); });
+    axios.post('http://localhost:4001/events', event).catch((err) => { console.log("comment:"); console.log(err); });
+    axios.post('http://localhost:4002/events', event).catch((err) => { console.log("query:"); console.log(err); });
+    axios.post('http://localhost:4003/events', event).catch((err) => { console.log("moderator:"); console.log(err); });
 
     res.send({ status: "ok" });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,11 +16,7 @@ app.get('/posts', (req, res) => {
 });
 
 
-app.post('/events', (req, res) => {
-    console.log("Received event:", req.body.type);
-    const { type, data } = req.body;
-
-
+const handleEvent = (type, data) => {
     if (type === "PostCreated") {
 
         const { id: postid, title } = data;
@@ -45,14 +42,46 @@ app.post('/events', (req, res) => {
         });
 
         comment.status = status;
-        comment.comment = content; 
+        comment.comment = content;
 
     }
+}
 
+
+
+app.post('/events', (req, res) => {
+    console.log("Received event:", req.body.type);
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
+    console.log('Checking events');
+
+    try {
+        const res = await axios.get("http://localhost:4005/events");
+        if (!res.data || res.data.length === 0) {
+            console.log('no event to parse');
+        } else {
+            
+            console.log(`Pasting ${res.data.length} events...`);
+            for (const event of res.data) {
+                try {
+                    console.log("Processing event:", event.type);
+                    handleEvent(event.type, event.data);
+
+                } catch (error) {
+                    console.log("event handling failed with error:", error);
+                }
+            }
+        }
+
+    } catch (error) {
+        console.log("an error occurred:", error);
+    }
+
     console.log('Listening on 4002');
 });
